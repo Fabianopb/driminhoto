042f870e198ec07daeaebc5b235d1a41d0af9055
Fabiano Brito <fabianopbrito@gmail.com>
2018-01-31 22:19:28 +0200

    Registers route before pointing the path to static resources.

--- server/server.js
+++ server/server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(path.resolve(__dirname, '..', 'build')));
-
 app.get('/test', (request, response) => {
   return response.status(200).send('route loaded successfully!');
 });
+
+app.use(express.static(path.resolve(__dirname, '..', 'build')));
 
 app.get('*', (request, response) => {
   response.sendFile(path.resolve(__dirname, '..', 'build', 'index.html'));
