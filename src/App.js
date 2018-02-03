a0f0dd3a5a1f535545817867f2963697f210bc0e
Fabiano Brito <fabianopbrito@gmail.com>
2018-02-03 15:08:16 +0200

    Creates a basic structure for the landing page.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
+import './index.scss';
 
 class App extends Component {
   render () {
     return (
       <div>
-        driminhoto app
+        <div className='top-bar'>Top bar</div>
+        <div className='hero-section'>Hero</div>
+        <div className='about-section'>About</div>
+        <div className='skills-section'>Skills</div>
+        <div className='projects-section'>Projects</div>
       </div>
     );
   }
